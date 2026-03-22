39eabc7645152c9959359ef90e6574386e4cf96f
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-03-22 18:38:42 -0300

    refactor: ajuste de código

--- App.js
+++ App.js
@@ -37,37 +37,3 @@ export default function App() {
     </>
   );
 }
-
-
-
-
-
-
-
-
-
-/*import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import LoadingScreen from './components/LoadingScreen';
-import RoomSelectionScreen from './pages/SelecionandoAsala';
-import { useAppLoader } from './hook/useAppLoader';
-
-export default function App() {
-  const isLoaded = useAppLoader(5000);
-
-  if (!isLoaded) {
-    return (
-      <>
-        <LoadingScreen />
-        <StatusBar style="light" />
-      </>
-    );
-  }
-
-  return (
-    <>
-      <RoomSelectionScreen />
-      <StatusBar style="light" />
-    </>
-  );
-}*/
